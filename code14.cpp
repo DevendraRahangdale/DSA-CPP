1af9e2046b0f207dcb118affd17edf5a6701eca6
Devendra Rahangdale <142200946+DevendraRahangdale@users.noreply.github.com>
2025-07-02 21:41:11 +0530

    Add files via upload

---
+++ code14.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+
+
+using namespace std;
+ 
+ int main(){
+
+    char ch;
+    cout<<"enter the character:";
+    cin>>ch;
+
+    int ascii=int(ch);
+
+    cout<<"ASCII value of "<<ch<<":"<<ascii;
+
+ }
