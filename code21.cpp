b72729f64061f929e71e3dc3533926393c2c58ba
Devendra Rahangdale <142200946+DevendraRahangdale@users.noreply.github.com>
2025-07-11 14:08:40 +0530

    Add files via upload

---
+++ code21.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+#include<unordered_map>
+
+using namespace std;
+
+int main(){
+    string str;
+    cout<<"string is:";
+    getline(cin,str);
+    unordered_map<char,int>mp;
+
+    for(auto ch:str){
+        mp[ch]++;
+    }
+
+for(auto it:mp){
+    cout<<it.first<<":"<<it.second<<endl;
+}
+}
